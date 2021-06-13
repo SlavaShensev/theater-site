7bb03a6079c3cb102bf54f8eb6e2bdb6cf964ccc
Slava <SlavaShensev@gmail.com>
2021-06-13 18:07:07 +0300

    menu header styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.scss';
 
-
 function App() {
     return (
 
@@ -11,12 +10,50 @@ function App() {
                 <div className="container">
                     <div className="header__body">
                         <nav className="header__menu">
-
+                            <ul className="menu menu__list">
+                                <li className="menu__item">
+                                    <a className="menu__link title-sm" href="#">
+                                        Главная
+                                    </a>
+                                </li>
+                                <li className="menu__item">
+                                    <a className="menu__link title-sm" href="#">
+                                        О театре
+                                    </a>
+                                </li>
+                                <li className="menu__item">
+                                    <a className="menu__link title-sm" href="#">
+                                        Наши постановки
+                                    </a>
+                                </li>
+                                <li className="menu__item">
+                                    <a className="menu__link title-sm" href="#">
+                                        Партнеры театра
+                                    </a>
+                                </li>
+                                <li className="menu__item">
+                                    <a className="menu__link title-sm" href="#">
+                                        Контакты
+                                    </a>
+                                </li>
+                            </ul>
+                            <div className="menu menu__contact">
+                                <a className="menu__number title-xs" href="tel:+7 495 123-45-67">
+                                    +7 495 123-45-67
+                                </a>
+                            </div>
                         </nav>
+                        <div className="header__content">
+                            <div className="header__first-screen">
+
+                            </div>
+                            <div className="header__second-screen">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </header>
-
 
         </div>
     );
